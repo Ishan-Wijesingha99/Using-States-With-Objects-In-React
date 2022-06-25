d27e7323faf9694380205cd597c65ae61bf777f7
Ishan <ishanwijes@gmail.com>
2022-06-25 19:41:52 +1000

    toggled the state of an object property

--- src/components/ContactCard.js
+++ src/components/ContactCard.js
@@ -3,37 +3,56 @@ import React from "react";
 
 
 export const ContactCard = function() {
+
+    // making an object the state variable
     const [contact, setContact] = React.useState({
-        firstName: "John",
-        lastName: "Doe",
-        phone: "+1 (719) 555-1212",
-        email: "itsmyrealname@example.com",
+        firstName: "Eren",
+        lastName: "Yeager",
+        phone: "+1800 Freedom",
+        email: "finallyfree@freemail.com",
         isFavorite: false
     })
-    
+
+
+    // changing a property inside that object state variable
     const toggleFavorite = function() {
-        console.log("Toggle Favorite");
+
+        // remember, whatever is returned by the setState function is what the new object will be. If that's the case, and we only want to change one property of the original object, we need to return the entire object with all it's properties, and only change one of them
+        setContact(contact => {
+            return {
+            // this is really cool, we are copying ALL the properties of the original object, including the properties we want to change!
+            ...contact,
+            // but, we then REWRITE the properties we want to change. JavaScript will give precedence to the last one
+            isFavorite: !contact.isFavorite
+            }
+        });
+
     }
+
+
+    // conditional rendering using ternary operator
+    let starIconBoolean = contact.isFavorite ? 'images/star-filled.png' : 'images/star.png'; 
     
 
-
+    
     return (
         <main>
             <article className="card">
-                <img src="./images/user.png" className="card--image" />
+                <img src="images/eren-yeager.png" className="card--image" />
+
                 <div className="card--info">
                     <img 
-                        src={`../images/star-empty.png`} 
+                        src={starIconBoolean}
                         className="card--favorite"
                         onClick={toggleFavorite}
                     />
-                    <h2 className="card--name">
-                        John Doe
-                    </h2>
-                    <p className="card--contact">+1 (719) 555-1212</p>
-                    <p className="card--contact">itsmyrealname@example.com</p>
+
+                    <h2 className="card--name">{contact.firstName} {contact.lastName}</h2>
+
+                    <p className="card--contact">{contact.phone}</p>
+
+                    <p className="card--contact">{contact.email}</p>
                 </div>
-                
             </article>
         </main>
     )
